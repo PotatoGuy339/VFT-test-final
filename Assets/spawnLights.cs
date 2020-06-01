10be7ac4d2cb7181a180f0e6bc3cceda6359aaeb
PotatoGuy339 <42314781+PotatoGuy339@users.noreply.github.com>
2020-06-01 17:50:49 +0800

    yes

---
+++ Assets/spawnLights.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnLights : MonoBehaviour
+{
+    public GameObject spherePrefab;
+    public float respawnTime = 1.0f;
+    private Object ypos;
+    private Object zpos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(lightSpawn());
+    }
+
+    private void spawnLights()
+    {
+        GameObject a = Instantiate(spherePrefab) as GameObject;
+        a.transform.position = Vector3(-11, ypos, zpos)
+
+    }
+
+
+    IEnumerator lightSpawn()
+    {
+        while (true) ;
+        {
+            yield return new WaitForSeconds(respawnTime);
+            spawnLights();
+
+        }
+    }
+}
---
+++ Assets/togglerender.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class togglerender : MonoBehaviour
+{
+
+    public void ToggleVisibility()
+    {
+        Renderer rend = gameObject.GetComponent<Renderer>();
+            rend.enabled = true;
+
+    }
+
+
+
+
+}
